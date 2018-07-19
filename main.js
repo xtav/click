8c492525ecddff6587887c86f7caa95aa5e45a76
xtav <40044891+xtav@users.noreply.github.com>
2018-07-18 19:23:35 -0700

    again

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // This urls is from airtable from the authentication section
-var airtable_list_url = 'https://api.airtable.com/v0/appM9DH9mXour47dP/Table%201?api_key=key92U04PjpzSuRxg'
+var airtable_list_url = 'https://api.airtable.com/v0/appM9DH9mXour47dP/Table%201?api_key=key92U04PjpzSuRxg';
 
 // This is where we get the JSON data from Airtable!
 
